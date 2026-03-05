b8b3e380232403f374c609ee3f76d01c0793eda0
fariszigger <faris.zigger@gmail.com>
2026-03-05 18:45:21 +0700

    Fix UserSeeder to use Spatie assignRole

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,143 +15,143 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         // Admin User
-        User::firstOrCreate(
+        $admin = User::firstOrCreate(
             ['email' => 'admin@bprpuri.com'],
             [
                 'username' => 'admin',
                 'name' => 'Faris Muhammad',
                 'password' => Hash::make('admin1234'),
-                'role' => 'Admin',
                 'code' => 'FR',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $admin->assignRole('Admin');
 
         // User AO AR
-        User::firstOrCreate(
+        $ao_ar = User::firstOrCreate(
             ['email' => 'ao_ar@bprpuri.com'],
             [
                 'username' => 'ar1234',
                 'name' => 'Moch. Arif Priyadi',
                 'password' => Hash::make('ar123456*'),
-                'role' => 'Kabag',
                 'code' => 'AR',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_ar->assignRole('Kabag');
 
         // User AO BW
-        User::firstOrCreate(
+        $ao_bw = User::firstOrCreate(
             ['email' => 'ao_bw@bprpuri.com'],
             [
                 'username' => 'bw777',
                 'name' => 'Kohar Hari Wibowo',
                 'password' => Hash::make('bw123456*'),
-                'role' => 'AO',
                 'code' => 'BW',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_bw->assignRole('AO');
 
         // User AO KS
-        User::firstOrCreate(
+        $ao_ks = User::firstOrCreate(
             ['email' => 'ao_ks@bprpuri.com'],
             [
                 'username' => 'ks4467',
                 'name' => 'Kusmargianto',
                 'password' => Hash::make('123123ks*'),
-                'role' => 'AO',
                 'code' => 'KS',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_ks->assignRole('AO');
 
         // User AO SH
-        User::firstOrCreate(
+        $ao_sh = User::firstOrCreate(
             ['email' => 'ao_sh@bprpuri.com'],
             [
                 'username' => 'sh999',
                 'name' => 'Slamet Hariyadi',
                 'password' => Hash::make('999sh*'),
-                'role' => 'AO',
                 'code' => 'SH',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_sh->assignRole('AO');
 
         // User AO MA
-        User::firstOrCreate(
+        $ao_ma = User::firstOrCreate(
             ['email' => 'ao_ma@bprpuri.com'],
             [
                 'username' => 'asrory06',
                 'name' => 'Mohammad Asrory',
                 'password' => Hash::make('ma123123*'),
-                'role' => 'AO',
                 'code' => 'MA',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_ma->assignRole('AO');
 
         // User AO AP
-        User::firstOrCreate(
+        $ao_ap = User::firstOrCreate(
             ['email' => 'ao_ap@bprpuri.com'],
             [
                 'username' => 'ap_mojosari1',
                 'name' => 'Anggradia Pratama',
                 'password' => Hash::make('mojosari123*'),
-                'role' => 'AO',
                 'code' => 'AP',
                 'office_branch' => 'Kantor Kas Mojosari',
                 'status' => 'active',
             ]
         );
+        $ao_ap->assignRole('AO');
 
         // User AO HS
-        User::firstOrCreate(
+        $ao_hs = User::firstOrCreate(
             ['email' => 'ao_hs@bprpuri.com'],
             [
                 'username' => 'hendrik111*',
                 'name' => 'Hendrik Sulistiono',
                 'password' => Hash::make('hendrik_mojosari1*'),
-                'role' => 'AO',
                 'code' => 'HS',
                 'office_branch' => 'Kantor Kas Mojosari',
                 'status' => 'active',
             ]
         );
+        $ao_hs->assignRole('AO');
 
         // User AO GI
-        User::firstOrCreate(
+        $ao_gi = User::firstOrCreate(
             ['email' => 'ao_gi@bprpuri.com'],
             [
                 'username' => 'gi8765*',
                 'name' => 'Gilang Isbiantoro Putra',
                 'password' => Hash::make('gi7777*'),
-                'role' => 'AO',
                 'code' => 'GI',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_gi->assignRole('AO');
 
         // User AO RN
-        User::firstOrCreate(
+        $ao_rn = User::firstOrCreate(
             ['email' => 'ao_rn@bprpuri.com'],
             [
                 'username' => 'reno555*',
                 'name' => 'Alfilreno Keysha Bima Muhammad',
                 'password' => Hash::make('12309876*'),
-                'role' => 'AO',
                 'code' => 'RN',
                 'office_branch' => 'Kantor Pusat',
                 'status' => 'active',
             ]
         );
+        $ao_rn->assignRole('AO');
     }
 }
